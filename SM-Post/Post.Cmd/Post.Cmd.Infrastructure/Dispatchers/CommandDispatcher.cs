cd122e1b76a34f0080f6eb5ff8603b011f1e5481
Carlos <carlosbolorino@hotmail.com>
2023-03-18 18:02:16 +0100

    Command dispatch config

--- SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -3,7 +3,7 @@ using CQRS.Core.Infrastructure;
 
 namespace Post.Cmd.Infrastructure.Dispatchers
 {
-    internal class CommandDispatcher : ICommandDispatcher
+    public class CommandDispatcher : ICommandDispatcher
     {
         private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
 
@@ -19,7 +19,7 @@ namespace Post.Cmd.Infrastructure.Dispatchers
 
         public async Task SendAsync(BaseCommand command)
         {
-            if(_handlers.TryGetValue(command.GetType(), out var handler)) 
+            if (_handlers.TryGetValue(command.GetType(), out var handler))
             {
                 await handler(command);
             }
