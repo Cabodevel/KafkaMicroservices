84263a139e52b245edbb09250660b75e0e84dcff
Carlos <carlosbolorino@hotmail.com>
2023-03-18 11:08:14 +0100

    add commands and events

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/EditCommentComand.cs
@@ -0,0 +1,11 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands
+{
+    public class EditCommentComand : BaseCommand
+    {
+        public Guid CommandId { get; set; }
+        public string Comment { get; set; }
+        public string UserName { get; set; }
+    }
+}
